1771dfd813cef0caf9c99ed7aa9c6dd4c7a06a0a
vaclavloubek <vaclav.loubek@gmail.com>
2026-09-18 18:44:26 +0200

    Make Stripe webhook regression import Node-compatible

--- lib/stripe-webhook.ts
+++ lib/stripe-webhook.ts
@@ -1,5 +1,5 @@
 import { createHmac, timingSafeEqual } from 'node:crypto';
-import { billingRouteForCountry } from '@/lib/billing-region';
+import { billingRouteForCountry } from './billing-region.ts';
 
 const SIGNATURE_TOLERANCE_SECONDS = 5 * 60;
 const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
